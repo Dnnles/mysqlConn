f680715b92c7ce01091831fad5fcbaa7c2054ded
xianzhicc <xianzhi1012@163.com>
2021-03-30 16:34:27 +0800

    添加项目文件。

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <mysql.h>
+#include <stdio.h>
+
+#define IP_ADDR "123.56.85.70"
+#define USER_NAME "root"
+#define DB_PASSWORD "xianzhi1012"
+#define DB_NAME "book"
+MYSQL* conn = NULL;
+
+int main(void) {
+	conn = mysql_init(NULL);
+
+	if (!mysql_real_connect(conn, IP_ADDR, USER_NAME, DB_PASSWORD, DB_NAME, 0, NULL, 0)) {
+		printf("%s\n", mysql_error(conn));
+		printf("连接数据库服务器失败\n");
+	} else {
+		printf("成功");
+	}
+	return 0;
+}
